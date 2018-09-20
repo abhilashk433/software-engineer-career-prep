02fcb4c5c833d4cf362f0321cab4bd4908cb07c3
Max Li <maxlivinci@gmail.com>
2018-09-20 17:16:32 +0800

    add Reverse Bits

---
+++ Technical Questions/Binary/190. Reverse Bits.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+
+// n & 1 => last digit 0 or 1
+// n >> 1 => remove last digit
+// n << 1 => add 0 to the last digit
+const reverseBits = n => {
+  let result = 0;
+
+  for (let i = 0; i < 32; i++) {
+    result <<= 1;
+    if ((n & 1) === 1) result++;
+    n >>= 1;
+  }
+
+  return result >>> 0;
+};

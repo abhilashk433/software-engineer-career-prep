d01b0ad4a6b03e292f6036007b561b11c6ce9607
Max Li <maxlivinci@gmail.com>
2018-09-20 10:45:51 +0800

    add Counting Bits

---
+++ Technical Questions/Binary/338. Counting Bits.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {number[]}
+ */
+
+// if num = 5
+//   0
+//   1
+//  10
+//  11
+// 100
+// 101
+const countBits = num => {
+  const counts = [0];
+  for (let i = 0; i < num + 1; i++) {
+    counts[i] = counts[i >> 1] + (i & 1);
+  }
+  return counts;
+};

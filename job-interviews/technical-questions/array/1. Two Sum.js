d8ce5b3365b711f99f7129de8b1b6791d9a56dcb
Max Li <maxlivinci@gmail.com>
2018-09-24 10:13:44 +0800

    update 1. Two Sum

--- job-interviews/technical-questions/array/1. Two Sum.js
+++ job-interviews/technical-questions/array/1. Two Sum.js
@@ -5,15 +5,24 @@
  */
 // Complicity => O(n)
 var twoSum = function(nums, target) {
-  const map = {}; // { 7: 0 }
+  const map = {};
+
   for (let i = 0; i < nums.length; i++) {
     const currentNumber = nums[i];
-    const parter = target - nums[i];
+    const counterPart = target - nums[i];
 
+    /**
+     *
+     * Logic: (from the perspective of each number)
+     *
+     * 1. I will try use my identity as key to find my counterPart in the map. => line 23
+     * 2. If I cannot find one, I will put my desire counterPart as key and my address as value
+     *    to the map and hope next number will able to find me => line 25
+     */
     if (map[currentNumber] !== undefined) {
-      return [map[currentNumber], i];
+      return [map[currentNumber], i]; // line 23
     } else {
-      map[parter] = i;
+      map[counterPart] = i; // line 25
     }
   }
 };

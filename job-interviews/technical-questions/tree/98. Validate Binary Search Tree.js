2bb83ed74dca7a9f68bb1079ee2c1cde5565d4e4
Max Li <maxlivinci@gmail.com>
2018-09-25 17:57:59 +0800

    update 98. Validate Binary Search Tree

--- job-interviews/technical-questions/tree/98. Validate Binary Search Tree.js
+++ job-interviews/technical-questions/tree/98. Validate Binary Search Tree.js
@@ -1,15 +1,4 @@
 /**
-
- * Given a binary tree, determine if it is a valid binary search tree (BST).
-
- Assume a BST is defined as follows:
-
- The left subtree of a node contains only nodes with keys less than the node's key.
- The right subtree of a node contains only nodes with keys greater than the node's key.
- Both the left and right subtrees must also be binary search trees.
-
- solution: https://www.youtube.com/watch?v=NjTqDotq6D0
-
  * Definition for a binary tree node.
  * function TreeNode(val) {
  *     this.val = val;
@@ -20,11 +9,13 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = root => helper(root, null, null);
+const isValidBST = root => helper(root, null, null)
 
-var helper = (root, min, max) => {
-  if (root === null) return true;
-  if (min !== null && root.val <= min) return false;
-  if (max !== null && root.val >= max) return false;
-  return helper(root.left, min, root.val) && helper(root.right, root.val, max);
-};
+const helper = (root, min, max) => {
+  if (root === null) return true
+  if (min !== null && root.val <= min) return false
+  if (max !== null && root.val >= max) return false
+  const leftValidBST = helper(root.left, min, root.val)
+  const rightValidBST = helper(root.right, root.val, max)
+  return leftValidBST && rightValidBST
+}
